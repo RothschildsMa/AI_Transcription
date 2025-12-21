210dc0964bfcfa914f8b9bb67b9d06b45b92da29
RothschildsMa <unityidma@gmail.com>
2025-12-21 16:40:37 +0900

    style: format code with black

--- src/ai_transcription/core/utils.py
+++ src/ai_transcription/core/utils.py
@@ -1,8 +1,10 @@
 # src/ai_transcription/core/utils.py
 
+
 def add(a: int, b: int) -> int:
     """2つの数値を足し算する関数"""
     return a + b
+
 
 def subtract(a: int, b: int) -> int:
     """2つの数値を引き算する関数"""
